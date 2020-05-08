41d8a1053876679465a811842a46b933eab27335
FCTESLA <fcamacho888@gmail.com>
2020-05-08 11:25:54 -0700

    seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Baconator", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mac N' Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("El Diablo Burger", true);
+
